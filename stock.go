e705513548d25706759b34c7d005920b256aef2a
Jordan Hurwich <github@hurwichmail.com>
2015-10-11 11:50:52 -0700

    Added structs for markit response and general refactoring of Markit structs for cleanliness

--- stock.go
+++ stock.go
@@ -11,94 +11,22 @@ import (
 	"time"
 )
 
+// Stock object for manages all data accesses for a specific stock symbol
 type Stock struct {
-	symbol string
+	Symbol string
 }
 
+// Stock constructor
 func NewStock(sym string) *Stock {
 	return &Stock{
-		symbol: sym,
+		Symbol: sym,
 	}
-}
-
-const chartAPIURL string = "http://dev.markitondemand.com/Api/v2/InteractiveChart/json"
-
-type ChartAPIRequest struct {
-	stock     *Stock
-	startDate string
-	endDate   string
-	url       string
-}
-
-type ChartAPIRequestParams struct {
-	Normalized   bool
-	NumberOfDays int
-	DataPeriod   string
-	Elements     []Element
-}
-
-type Element struct {
-	Symbol string
-	Type   string
-	Params []string `json:",omitempty"`
-}
-
-type ChartAPIResponse struct {
-}
-
-func NewChartAPIRequest(s *Stock, start time.Time, end time.Time) (*ChartAPIRequest, error) {
-	request := &ChartAPIRequest{
-		stock:     s,
-		startDate: start.Format("2006-01-02T15:04:05-00"), // formatted like 2011-06-01T00:00:00-00
-		endDate:   end.Format("2006-01-02T15:04:05-00"),
-	}
-	request.url = chartAPIURL
-
-	params := ChartAPIRequestParams{
-		Normalized:   false,
-		NumberOfDays: 30, // TODO change!
-		DataPeriod:   "Day",
-		Elements: []Element{
-			Element{
-				Symbol: s.symbol,
-				Type:   "price",
-				Params: []string{"ohlc"},
-			},
-			Element{
-				Symbol: s.symbol,
-				Type:   "volume",
-			},
-		},
-	}
-
-	jsonStr, err := json.Marshal(params)
-	request.url = fmt.Sprintf("%s?parameters=%s", request.url, jsonStr)
-
-	fmt.Printf("request.url:\n%s\n\n", request.url)
-
-	resp, err := http.Get(request.url)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		return nil, errors.New(resp.Status)
-	}
-
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println("response Status:", resp.Status)
-	fmt.Println("response Headers:", resp.Header)
-	fmt.Println("response Body:", string(body))
-
-	return request, nil
-
 }
 
 func PollNewData(symbol string) (string, error) {
 	stock := NewStock(symbol)
-	fmt.Printf("Stock:\n %+v \n", stock)
 
-	request, err := NewChartAPIRequest(
+	request, err := NewMarkitChartAPIRequest(
 		stock,
 		time.Now().AddDate(0, 0, -3),
 		time.Now())
@@ -106,7 +34,118 @@ func PollNewData(symbol string) (string, error) {
 	if err != nil {
 		panic(err)
 	}
+	request.Request()
+
+	return "", nil
+}
+
+/*  - - - - - Markit  - - - - - */
+
+const markitChartAPIURL string = "http://dev.markitondemand.com/Api/v2/InteractiveChart/json"
+
+// Constructor for MarkitChartAPIRequests
+func NewMarkitChartAPIRequest(s *Stock, start time.Time, end time.Time) (*MarkitChartAPIRequest, error) {
+	request := &MarkitChartAPIRequest{
+		Stock:     s,
+		StartDate: start.Format("2006-01-02T15:04:05-00"), // formatted like 2011-06-01T00:00:00-00
+		EndDate:   end.Format("2006-01-02T15:04:05-00"),
+		Url:       markitChartAPIURL,
+	}
+
+	// use object to build json parameters for url
+	params := MarkitChartAPIRequestParams{
+		Normalized:   false,
+		NumberOfDays: 30, // TODO change!
+		DataPeriod:   "Day",
+		Elements: []Element{
+			Element{
+				Symbol: s.Symbol,
+				Type:   "price",
+				Params: []string{"ohlc"},
+			},
+			Element{
+				Symbol: s.Symbol,
+				Type:   "volume",
+			},
+		},
+	}
+	jsonStr, err := json.Marshal(params)
+	request.Url = fmt.Sprintf("%s?parameters=%s", request.Url, jsonStr)
+	fmt.Printf("request.Url:\n%s\n\n", request.Url)
+
+	return request, err
+}
+
+func (request *MarkitChartAPIRequest) Request() (*MarkitChartAPIResponse, error) {
+	response, err := http.Get(request.Url)
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+	if response.StatusCode != http.StatusOK {
+		return nil, errors.New(response.Status)
+	}
+
+	body, _ := ioutil.ReadAll(response.Body)
+	fmt.Println("response Status:", response.Status)
+	fmt.Println("response Headers:", response.Header)
+	fmt.Println("response Body:", string(body))
+
+	if err != nil {
+		panic(err)
+	}
 
 	fmt.Printf("\n\nREQUEST:\n %+v \n", request)
-	return "", nil
+	return nil, nil
+}
+
+// Markit API request format and supporting structs
+type MarkitChartAPIRequest struct {
+	Stock     *Stock
+	StartDate string
+	EndDate   string
+	Url       string
+}
+type MarkitChartAPIRequestParams struct {
+	Normalized   bool
+	NumberOfDays int
+	DataPeriod   string
+	Elements     []Element
+}
+type Element struct {
+	Symbol     string
+	Type       string
+	Params     []string   `json:",omitempty"`
+	Currency   string     `json:",omitempty"`
+	Timestamp  string     `json:"TimeStamp,omitempty"`
+	Dataseries Dataseries `json:"DataSeries"`
+}
+type Dataseries struct {
+	Open   Data `json:"open,omitempty"`
+	High   Data `json:"high,omitempty"`
+	Low    Data `json:"low,omitempty"`
+	Close  Data `json:"close,omitempty"`
+	Volume Data `json:"volume,omitempty"`
+}
+type Data struct {
+	Min     float32   `json:"min"`
+	Max     float32   `json:"max"`
+	MaxDate time.Time `json:"maxDate"`
+	MinDate time.Time `json:"minDate"`
+	Values  []float32 `json:"values"`
+}
+
+// Markit API response format
+type MarkitChartAPIResponse struct {
+	Labels    MarkitChartAPIResponseLabels
+	Positions []float32
+	Dates     []time.Time
+	Elements  []Element
+}
+type MarkitChartAPIResponseLabels struct {
+	Dates      []string `json:"dates"`
+	Pos        []string `json:"pos"`
+	Priorities []string `json:"priorities"`
+	Text       []string `json:"text"`
+	UtcDates   []string `json:"utcdates"`
 }
