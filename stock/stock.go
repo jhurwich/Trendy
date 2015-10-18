eba5dbf4e4fc013f5663b98cc208be174fc6c46e
Jordan Hurwich <github@hurwichmail.com>
2015-10-18 10:26:47 -0700

    Stub out new methods for Stock including Populate, Analyze, and Range

--- stock/stock.go
+++ stock/stock.go
@@ -8,16 +8,66 @@ import (
 	"time"
 )
 
-// Stock object for manages all data accesses for a specific stock symbol
-type Stock struct {
-	Symbol string
+// Span and Measure objects for calculating and saving trend data
+type Span []Measure
+type Measure struct {
+	time  time.Time
+	value float32
+	trend float32
 }
 
-// Stock constructor
+// Stock object for manages all data accesses for a specific stock symbol
+type Stock struct {
+	Symbol   string
+	Measures Span
+}
+
 func NewStock(sym string) *Stock {
 	return &Stock{
 		Symbol: sym,
 	}
+}
+
+// query daily measure data between times provided for a stock
+func (s *Stock) Range(startDate time.Time, endDate time.Time) *Span {
+	if startDate.IsZero() && endDate.IsZero() {
+		// zero time is used as sentinel to query entire range available
+		//TODO(jhurwich) implement
+	}
+
+	return &Span{}
+}
+func (s *Stock) RangeAll() *Span {
+	// zero time is used as sentinel to query all data available
+	return s.Range(time.Time{}, time.Time{})
+}
+
+// populate daily measure data (value field) between times provided
+func (s *Stock) Populate(startDate time.Time, endDate time.Time) error {
+	if startDate.IsZero() && endDate.IsZero() {
+		// zero time is used as sentinel to query entire range available
+		//TODO(jhurwich) implement
+	}
+
+	return nil
+}
+func (s *Stock) PopulateAll() error {
+	// zero time is used as sentinel to populate all data
+	return s.Populate(time.Time{}, time.Time{})
+}
+
+// calculate trend for measures between times provided
+func (s *Stock) Analyze(startDate time.Time, endDate time.Time) error {
+	if startDate.IsZero() && endDate.IsZero() {
+		// zero time is used as sentinel to query entire range available
+		//TODO(jhurwich) implement
+	}
+
+	return nil
+}
+func (s *Stock) AnalyzeAll() error {
+	// zero time is used as sentinel to analyze all data
+	return s.Analyze(time.Time{}, time.Time{})
 }
 
 func PollNewData(symbol string) (string, error) {
